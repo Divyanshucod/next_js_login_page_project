1e724cdde35c3baf7be5d5a2c82c13f90ef20e8b
Divyanshucod <prajapatidivyanshu90@gmail.com>
2024-11-21 13:18:27 +0530

    added logout button and api

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,21 @@
+"use client"
+import axios from "axios";
+
 export default function Profile(){
-    
+    const logOut = async ()=>{
+        try {
+            const response = await axios.get('/api/users/logout');
+            console.log(response.data);
+
+        } catch (error:any) {
+            console.log(error.message);
+            
+        }
+    }
     return (
         <div className="flex flex-col items-center justify-center py-2 h-screen">
             <h1>Profile</h1>
+            <button onClick={logOut}>Log Out</button>
         </div>
     )
 }
